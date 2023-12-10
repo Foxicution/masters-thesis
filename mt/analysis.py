adcb99ccda0538a3f4c2568bebe54fb0e1d65b8d
Foxicution <foxicution@gmail.com>
2023-12-10 17:14:04 +0000

    added feature analysis modules

---
+++ mt/analysis.py
@@ -0,0 +1,37 @@
+from mt.definitions import DATA_DIR
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_DIR / "final_non_scaled.csv", index_col=0)
+    print(df.columns)
+    print(df.describe().to_csv("desc_stat.csv"))
+    print(df.isnull().sum())
+    df = pd.read_csv(DATA_DIR / "final.csv", index_col=0)
+    # Distribution of Y values
+    sns.histplot(df["Y"], kde=True)
+    plt.title("Distribution of Y")
+    plt.savefig("hist.png")
+    # # Correlation matrix
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(df.corr(), annot=True, fmt=".2f")
+    plt.tight_layout()
+    plt.title("Correlation Matrix")
+    plt.savefig("corr.png")
+    selected_features = [
+        "code",
+        "comments",
+        "complexity",
+        "dependencies",
+        "pylint_rating",
+        "pylint_message_count",
+        "comment_tokens",
+    ]
+    # # Pair plot for a subset of features (select features relevant to your analysis)
+    sns.pairplot(df[["Y", "Y_scale", "Y_standard_scaled"] + selected_features])
+    plt.savefig("pair.png")
+    # Box plot for checking outliers in Y
+    # sns.boxplot(data=df, y="Y")
+    # plt.title("Box Plot of Y for Outliers")
+    # plt.savefig("boxplot.png")
---
+++ mt/prediction_feat_importance.py
@@ -0,0 +1,61 @@
+from mt.definitions import DATA_DIR
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_DIR / "final.csv", index_col=0)
+    # Assuming df is your DataFrame and 'Y_scaled' is the target
+    X = df.drop(
+        columns=["Y", "Y_scale", "Y_scaled", "commit_index"]
+    )  # or any non-feature columns
+    y = df["Y_scaled"]  # or 'Y' or 'Y_standard_scaled' based on your target definition
+
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    # Initialize and train the linear regression model
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    # Make predictions
+    y_pred = model.predict(X_test)
+
+    # Metrics
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    metrics_df = pd.DataFrame({"Mean Squared Error": [mse], "R^2 Score": [r2]})
+    metrics_df.to_csv("metrics.csv")
+
+    # Feature Importances
+    rf_importances = model.feature_importances_
+    importances_df = pd.DataFrame(
+        {"Feature": X.columns, "Importance": rf_importances}
+    ).sort_values(by="Importance", ascending=False)
+
+    importances_df.to_csv("importances.csv")
+    # # Extracting feature importances (coefficients)
+    # feature_importances = model.coef_
+
+    # # Create a DataFrame for feature importances
+    # importances_df = pd.DataFrame(
+    #     {"Feature": X.columns, "Importance": feature_importances}
+    # )
+    # # Sort by importance
+    # importances_df.sort_values(by="Importance", ascending=False, inplace=True)
+
+    # importances_df.to_csv("importances.csv")
+
+    plt.scatter(y_test, y_pred, alpha=0.5)
+    plt.title("Actual vs Predicted Values")
+    plt.xlabel("Actual Values")
+    plt.ylabel("Predicted Values")
+    plt.plot(
+        [y_test.min(), y_test.max()], [y_test.min(), y_test.max()], "k--"
+    )  # Diagonal line
+    plt.savefig("pred.png")
